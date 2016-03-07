d6abe4df6c04b67ecf2945c20a553f0f2072de40
Wayne Fong <wfong2@caltech.edu>
2016-03-06 23:28:47 -0800

    test comment

--- player.cpp
+++ player.cpp
@@ -1,5 +1,5 @@
 #include "player.h"
-
+/* Test by Wayne */
 /*
  * Constructor for the player; initialize everything here. The side your AI is
  * on (BLACK or WHITE) is passed in as "side". The constructor must finish 
