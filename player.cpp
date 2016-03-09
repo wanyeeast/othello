bc2b5f88f83e868e951521f1232e1e1bcfb48ad4
Jeffrey Pak <jpak1099@gmail.com>
2016-03-09 04:06:07 -0800

    added some testing print lines for player.cpp, trying to find out what the seg fault is from. RIP sleep schedule

--- player.cpp
+++ player.cpp
@@ -1,10 +1,10 @@
 #include "player.h"
-#include <stdlib.h>  
-#include <vector> 
+#include <stdlib.h>
+#include <vector>
 /* Test by Wayne */
 /*
  * Constructor for the player; initialize everything here. The side your AI is
- * on (BLACK or WHITE) is passed in as "side". The constructor must finish 
+ * on (BLACK or WHITE) is passed in as "side". The constructor must finish
  * within 30 seconds.
  */
 Player::Player(Side side) {
@@ -23,7 +23,7 @@ Player::Player(Side side) {
         this->oppside = WHITE;
     }
 
-    /* 
+    /*
      * TODO: Do any initialization you need to do here (setting up the board,
      * precalculating things, etc.) However, remember that you will only have
      * 30 seconds.
@@ -49,34 +49,55 @@ Player::~Player() {
  * return NULL.
  */
 Move *Player::doMove(Move *opponentsMove, int msLeft) {
-    /* 
+    /*
      * TODO: Implement how moves your AI should play here. You should first
      * process the opponent's opponents move before calculating your own move
-     */ 
-
-    this->curBoard->doMove(opponentsMove, this->oppside);
+     */
+    std::cerr << "msLeft: " << msLeft << std::endl;
+    if(opponentsMove != NULL)
+    {
+        cerr << "oppside: " << oppside << endl;
+        cerr << "opp move: " << (int)opponentsMove << endl;
+        this->curBoard->doMove(opponentsMove, this->oppside);
+    }
+    else
+    {
+        std::cerr << "opponent didn't have a move" << std::endl;
+    }
+    // vector<pair> Move
+    // std::pair <std::string,double> product1; 
     vector<Move> PossibleMoves = vector<Move>();
-
     if(curBoard->hasMoves(playerside))
     {
-        for (int i = 0; i < 8; i++) 
+        for (int i = 0; i < 8; i++)
         {
-            for (int j = 0; j < 8; j++) 
+            for (int j = 0; j < 8; j++)
             {
                 Move move(i, j);
-                if (this->curBoard->checkMove(&move, this->playerside)) 
+                if (this->curBoard->checkMove(&move, this->playerside))
                 {
                     PossibleMoves.push_back(move);
                 }
             }
         }
-    }   
+    }
 
+    std::cerr << "possible moves: " << PossibleMoves.size() << std::endl;
     if(PossibleMoves.size() > 0)
     {
         int random = rand() % PossibleMoves.size();
+        for(int i = 0; i < (int)PossibleMoves.size(); i++)
+        {
+            std::cerr << " " << (int)&PossibleMoves[i];
+        }
+        std::cerr << std::endl;
 
+        // std::cerr << "msLeft: " << msLeft << std::endl;
+        // Move * temp =  &PossibleMoves[random];
+        // std::cerr << "Move Taken: " << (int)temp << std::endl;
+        this->curBoard->doMove(&PossibleMoves[random], this->playerside);
         return &PossibleMoves[random];
     }
+    std::cerr << "reach" << std::endl;
     return NULL;
 }
